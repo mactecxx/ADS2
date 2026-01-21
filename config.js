02cc030c3aa51d058042072a877d2c46190df94d
mactecxx <140248271+mactecxx@users.noreply.github.com>
2026-01-21 11:30:24 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,6 @@
+// config.js
+// ONLY Keys go here. Do NOT add 'const supabase = ...' here.
 const _CONFIG = {
     supabaseUrl: "https://botuspjtaqkqibyqkacd.supabase.co",
-    supabaseKey: "sb_publishable_BkBSWY_1ii3OivvOWeCYoQ_slnEL-HN"
+    supabaseKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJvdHVzcGp0YXFrcWlieXFrYWNkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njg5NjIyODYsImV4cCI6MjA4NDUzODI4Nn0.CpWLFp1rS_lZz3KtoD1ctcJA29KQ21NOce-g2-6Vt68"
 };
