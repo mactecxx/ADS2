784b6acda185870a5b68792fa96f4da8264e3b40
mactecxx <140248271+mactecxx@users.noreply.github.com>
2026-02-17 20:59:09 +0530

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-// config.js
-// ONLY Keys go here. Do NOT add 'const supabase = ...' here.
-const _CONFIG = {
-    supabaseUrl: "https://botuspjtaqkqibyqkacd.supabase.co",
-    supabaseKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJvdHVzcGp0YXFrcWlieXFrYWNkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njg5NjIyODYsImV4cCI6MjA4NDUzODI4Nn0.CpWLFp1rS_lZz3KtoD1ctcJA29KQ21NOce-g2-6Vt68"
-};
